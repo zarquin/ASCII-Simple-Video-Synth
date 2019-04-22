2ebdb37eda87d836217b4a47bddfec348bf84284
Alwyn Nixon-Lloyd <32872980+zarquin@users.noreply.github.com>
2019-04-22 22:45:42 +0800

    Update for making it nicer to use
    Added updated TouchOSC template.
    
    added modes to the Generators
    
    refactored the OSC callbacks

--- AASHF.py
+++ AASHF.py
@@ -51,26 +51,91 @@ def char_index_to_YX(screen, index):
     return ( lines, cols)
 
 class Generator:
-    def __init__(self, shape, increment, scale):
+    #modes
+    # 0 freerun
+    # 1 reset on line
+    # 2 reset on frame
+    # 3 oneshot, hold at 0 reset on line
+    # 4 oneshot, hold at 0 reset on frame
+    # 5 oneshot, hold at 1, rest on line
+    # 6 oneshot, hold at 1, reset on frame
+    def __init__(self, shape, increment=0.01, scale=255, offset=0,mode=0):
         self.shape = shape
         self.increment = increment
         self.value = 0.0
         self.wrap = 1.0
         self.scale = scale
+        self.offset = offset
+        self.mode = mode
+        self.run = True
         return
+
     def reset(self, new_value=0):
         self.value = new_value
+        self.run=True
         return
+
+    def calculate_and_limit(self):
+        ret_val = 0
+        ret_val = int(self.value * self.scale)
+        ret_val = ret_val+self.offset
+        if ret_val > 255:
+            ret_val = 255
+        if ret_val <0:
+            ret_val = 0
+        return ret_val
+
     def next(self):
+
+        if self.run is False:
+        # if we're in a "oneshot" mode return the current value.
+            return self.calculate_and_limit()
         self.value +=self.increment
         if self.value>self.wrap:
             self.value = (self.value - self.wrap)
+            #if we're in oneshot mode, we need to do logic here.
+            if self.mode in [3,4]:  #oneshot return to 0.0
+                self.value = 0.0
+                self.run = False
+            if self.mode in [5,6]:  #oneshot hold at 1.0
+                self.value = 1.0
+                self.run = False
         if self.value > self.wrap:
             #stop it blowing up
             self.value = 0.0
         if self.value < 0.0:
-            self.value =0.0
-        return (self.value*self.scale)
+            self.value =0.0  
+        return self.calculate_and_limit()
+
+    def set_mode(self, new_mode):
+        new_mode = int(new_mode)
+        if new_mode > 6:
+            new_mode = 6
+        if new_mode < 0:
+            new_mode = 0
+        self.mode = new_mode
+        return
+
+    def line_reset(self):
+        if self.mode in [1,3,5]:
+            self.run = True
+            self.reset(0.0)
+        return
+
+    def frame_reset(self):
+        if self.mode in [2,4,6]:
+            self.run = True
+            self.reset(0.0)
+        return
+
+    def set_offset(self,new_offset):
+        new_offset = int(new_offset)
+        if new_offset > 255:
+            new_offset = 255
+        if new_offset <0:
+            new_offset = 0
+        self.offset = new_offset
+        return
 
     def set_increment(self, new_increment):
         #increment has to be less than 2.0
@@ -78,7 +143,13 @@ class Generator:
             new_increment = 1.9
         self.increment = new_increment
         return
+
     def set_scale(self, new_scale):
+        new_scale = int(new_scale)
+        if new_scale > 255:
+            new_scale = 255
+        if new_scale <0:
+            new_scale = 0
         self.scale = new_scale
         return
 
--- ASCII-Simple-Video-Synth.py
+++ ASCII-Simple-Video-Synth.py
@@ -13,71 +13,47 @@ redGen = Generator(1.0,0.01,255.0)
 bluGen = Generator(1.0,0.002,255.0)
 grnGen = Generator(1.0,0.001,255.0)
 
-red_offset = 80.0
-green_offset = 0.0
-blue_offset = 0.0
 
-def red_speed_handler(unused_addr, args):
-    value = int(args)/220.0
-    redGen.set_increment(value)
+def speed_handler(unused_addr, args, val):
+    value = int(val)/240.0
+    args[0](value)
     return
 
-def green_speed_handler(unused_addr, args):
-    value = int(args)/220.0
-    grnGen.set_increment(value)
+def standard_handler(unused_addr, args, val):
+    value = int(val)
+    args[0](value)
     return
 
-def blue_speed_handler(unused_addr, args):
-    value = int(args)/220.0
-    bluGen.set_increment(value)
-    return
-
-def red_scale_handler(unused_addr, args):
-    value=int(args)
-    redGen.set_scale(value)
-    return
-
-def blue_scale_handler(unused_addr, args):
-    value=int(args)
-    bluGen.set_scale(value)
-    return
-
-def green_scale_handler(unused_addr, args):
-    value=int(args)
-    grnGen.set_scale(value)
-    return
-
-def red_offset_handler(unused_addr, args):
-    value=int(args)
-    global red_offset
-    red_offset = value
-    return
-
-def blue_offset_handler(unused_addr, args):
-    value=int(args)
-    global blue_offset
-    blue_offset = value
-    return
-
-def green_offset_handler(unused_addr, args):
-    value=int(args)
-    global green_offset
-    green_offset = value
-    return
 
 def get_colour():
-    red = int(redGen.next() + red_offset)
-    green = int (grnGen.next() + green_offset)
-    blue = int(bluGen.next() + blue_offset)
+    red = int(redGen.next() )
+    green = int (grnGen.next() )
+    blue = int(bluGen.next() )
 
     return AASHF.col255_from_RGB(red,green,blue)
 
+def frame_reset():
+    redGen.frame_reset()
+    grnGen.frame_reset()
+    bluGen.frame_reset()
+    return
+
+def line_reset():
+    redGen.line_reset()
+    grnGen.line_reset()
+    bluGen.line_reset()
+    return
+
 def draw_scene(screen):
+    frame_reset()
     for i in range( AASHF.char_count(screen)):
         this_pixel = get_colour()
         YX = AASHF.char_index_to_YX(screen, i)
+        if YX[1] == 0:
+            line_reset()
         screen.print_at(" ", YX[1], YX[0],colour=7, bg=this_pixel)
     screen.print_at("{:.2f} {:.2f} {:.2f} ".format(redGen.increment, grnGen.increment, bluGen.increment), 0,0,colour=7)
+    screen.print_at("{} {} {} ".format(redGen.mode, grnGen.mode, bluGen.mode), 25,0,colour=7)
     
     #screen.print_at("{} {} {}".format(redGen.increment, redGen.scale, red_offset), 0,3,colour=7)
     
@@ -92,29 +68,41 @@ def ds(screen):
         time.sleep(0.1)
     return
 
-def setup_OSC():
+def setup_OSC(new_ip, new_port):
     disp = dispatcher.Dispatcher()
-    disp.map("/red/speed",red_speed_handler)
-    disp.map("/red/offset", red_offset_handler)
-    disp.map("/red/scale", red_scale_handler)
-    disp.map("/green/speed",green_speed_handler)
-    disp.map("/green/offset", green_offset_handler)
-    disp.map("/green/scale", green_scale_handler)
-    disp.map("/blue/speed",blue_speed_handler)
-    disp.map("/blue/offset", blue_offset_handler)
-    disp.map("/blue/scale", blue_scale_handler)
+    disp.map("/red/mode",standard_handler, redGen.set_mode)
+    disp.map("/green/mode",standard_handler, grnGen.set_mode)
+    disp.map("/blue/mode",standard_handler, bluGen.set_mode)
+    disp.map("/red/offset", standard_handler, redGen.set_offset)
+    disp.map("/red/scale", standard_handler, redGen.set_scale)
+    disp.map("/green/offset", standard_handler, grnGen.set_offset)
+    disp.map("/green/scale", standard_handler, grnGen.set_scale)
+    disp.map("/blue/offset", standard_handler, bluGen.set_offset)
+    disp.map("/blue/scale", standard_handler, bluGen.set_scale)
+
+    disp.map("/red/speed",speed_handler,redGen.set_increment)
+    disp.map("/green/speed",speed_handler,grnGen.set_increment)
+    disp.map("/blue/speed",speed_handler,bluGen.set_increment)
 
     server = osc_server.ThreadingOSCUDPServer(
-            ("192.168.43.23",8000), disp)
+            (new_ip,new_port), disp)
     server_thread = threading.Thread(target = server.serve_forever)
     server_thread.start()
     return server, server_thread
 
 
 
-s = setup_OSC()
-Screen.wrapper(ds)
-s[0].shutdown()
-quit()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+      help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=5005,
+      help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    s = setup_OSC(args.ip, args.port)
+    Screen.wrapper(ds)
+    s[0].shutdown()
+    quit()
 
 
